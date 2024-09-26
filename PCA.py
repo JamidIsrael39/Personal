a0f201392f59156d10f2e77929cd4f0ce1e0091e
JamidIsrael39 <israel.93.ig@outlook.com>
2024-09-25 20:29:48 -0600

    Agregando archivo

---
+++ PCA.py
@@ -0,0 +1,11 @@
+def image_projection(testimage, im_average, eigen_images, row_num, col_num):
+    """
+    Funcion que aplica el algoritmo de la proyeccion de una eigenimagen
+    como en el paper de Turck y Pentland: 
+    http://www.vision.jhu.edu/teaching/vision08/Handouts/case_study_pca1.pdf
+    
+    Entradas:
+        testimage  : Imagen que se proyectara en el eigenespacio
+        im_average : Imagen promedio del eigenespacio generado
+        
+    """
